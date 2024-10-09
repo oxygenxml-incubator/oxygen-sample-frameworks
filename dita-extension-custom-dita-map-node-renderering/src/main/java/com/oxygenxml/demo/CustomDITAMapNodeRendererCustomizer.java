81e22aa96bef66a7ed07314641255d41c0e22110
Bogdan Dumitru <bogdan_dumitru@sync.ro>
2024-10-09 14:49:17 +0300

    Update CustomDITAMapNodeRendererCustomizer.java

--- dita-extension-custom-dita-map-node-renderering/src/main/java/com/oxygenxml/demo/CustomDITAMapNodeRendererCustomizer.java
+++ dita-extension-custom-dita-map-node-renderering/src/main/java/com/oxygenxml/demo/CustomDITAMapNodeRendererCustomizer.java
@@ -1,6 +1,9 @@
 package com.oxygenxml.demo;
 
-import java.net.URL;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import ro.sync.ecss.extensions.api.node.AuthorNode;
 import ro.sync.exml.workspace.api.editor.page.ditamap.DITAMapNodeRendererCustomizer;
@@ -9,10 +12,30 @@ import ro.sync.exml.workspace.api.editor.page.ditamap.DITAMapNodeRendererCustomi
  * Customize topicrefs rendering in the DITA Map view.
  */
 public class CustomDITAMapNodeRendererCustomizer extends DITAMapNodeRendererCustomizer {
+
+  private static final Logger logger = LoggerFactory.getLogger(CustomDITAMapNodeRendererCustomizer.class.getName());
+
+  private final AtomicInteger atomicInteger = new AtomicInteger();
+
+  @Override
+  public String customizeRenderedTopicrefTitle(AuthorNode topicref, String defaultRenderedTitle) {
+    logger.warn("customizeRenderedTopicrefTitle...");
+    String customizeRenderedTopicrefTitle = super.customizeRenderedTopicrefTitle(topicref, defaultRenderedTitle);
+    logger.warn("customizeRenderedTopicrefTitle: " +  customizeRenderedTopicrefTitle);
+
+    return customizeRenderedTopicrefTitle;
+  }
+
   @Override
   public String customizeComputedTopicrefTitle(AuthorNode topicref, AuthorNode targetTopicOrMap,
       String defaultComputedTitle) {
-    URL topicOrMapUrl = targetTopicOrMap.getXMLBaseURL();
-    return defaultComputedTitle + " (" + topicOrMapUrl + ")";
+    logger.warn("customizeComputedTopicrefTitle: " + defaultComputedTitle);
+
+    String badge = getBadge();
+    return badge + " " + defaultComputedTitle;
+  }
+
+  private String getBadge() {
+    return "#" + atomicInteger.incrementAndGet();
   }
 }
